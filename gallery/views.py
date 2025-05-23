5cafcd7802c8db9a1943c345891ee25de42c0951
Kikono1014 <vitalik.pyvovarchuk@gmail.com>
2025-05-23 12:58:14 +0300

    added gallery_view

--- gallery/views.py
+++ gallery/views.py
@@ -1,1 +1,10 @@
 from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from datetime import timedelta
+from .models import Image
+
+def gallery_view(request):
+    today = timezone.now().date()
+    thirty_days_ago = today - timedelta(days=30)
+    images = Image.objects.filter(created_date__gte=thirty_days_ago).order_by('-created_date')
+    return render(request, 'gallery.html', {'images': images})
